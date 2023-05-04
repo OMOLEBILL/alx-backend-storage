55d5d1fd066d8deaeb0f532739f44ba9b5f2ae29
bill omole <omolebill01@gmail.com>
2023-05-04 23:00:20 +0200

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- lets validate emails using triggers
+-- lets trigger
+DELIMITER $$
+CREATE TRIGGER validate BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END $$
+DELIMITER ;
