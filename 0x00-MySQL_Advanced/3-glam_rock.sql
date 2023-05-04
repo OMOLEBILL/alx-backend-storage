624921aa6b2567b376e835e6b84b2388aa3cfa66
bill omole <omolebill01@gmail.com>
2023-05-04 22:02:02 +0200

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- we use the iFNULL and Now function
 -- lets get the lifespan
-SELECT band_name, (IFNULL(split, YEAR(NOW())) - formed) AS lifespan
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE '%GLAM rock%';
+WHERE style LIKE '%GLAM rock%'
+ORDER BY lifespan DESC;
