273ccbc5f3a47f6245e526f71cb0b0d4a8e88d70
bill omole <omolebill01@gmail.com>
2023-05-09 14:37:04 +0200

    bugfix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,9 +2,8 @@
 """ We return a list of all documents """
 
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """ Mongo_collection is the collection we 
-        need to return the list of document"""
-        alldoc = mongo_collection.find()
-        return [doc for doc in alldoc]
+def list_all(mongo_collection):
+    """ Mongo_collection is the collection we
+    need to return the list of document"""
+    alldoc = mongo_collection.find()
+    return [doc for doc in alldoc]
