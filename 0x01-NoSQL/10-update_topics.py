1bdc13aa5db587ddc41a12d30eaf7479614fbbad
bill omole <omolebill01@gmail.com>
2023-05-09 23:54:39 +0200

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" We update the collection matching the given name
+    with the list of topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Lets add some topics """
+    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
