22e419679b1008f566b62acccd5c8f7572d7f3ba
bill omole <omolebill01@gmail.com>
2023-05-10 01:27:05 +0200

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+WE check summary of the methods in the log files
+"""
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    for item in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(item,
+              nginx_collection.count_documents({"method": item})))
+    print("{} status check".format(nginx_collection.count_documents(
+          {"method": "GET", "path": "/status"})))
